024ea13faa7c99b7d91a9c0ccaf4e77f48e74727
Kolade Afode <kolade.afode@yahoo.com>
2022-02-22 15:12:01 +0100

    complete exercises 8.24 & 8.25

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import Login from "./components/Login";
-import { useApolloClient } from "@apollo/client";
+import {
+  useQuery,
+  gql,
+  useMutation,
+  useSubscription,
+  useApolloClient,
+} from "@apollo/client";
+
+const BOOK_ADDED = gql`
+  subscription Subscription {
+    bookAdded {
+      title
+      published
+      id
+      author {
+        id
+        name
+        bookCount
+        born
+      }
+      genres
+    }
+  }
+`;
 
 const App = () => {
   const [page, setPage] = useState("authors");
@@ -13,6 +36,12 @@ const App = () => {
 
   const [error, setError] = useState(null);
   const client = useApolloClient();
+
+  useSubscription(BOOK_ADDED, {
+    onSubscriptionData: ({ subscriptionData }) => {
+      console.log(subscriptionData);
+    },
+  });
 
   console.log(error);
 
